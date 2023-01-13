738f8a0545a42ed44d07ab605b9e50f4b2abfca5
irtiza-chowdhury <irtizachowdhury5@gmail.com>
2023-01-13 22:22:14 +0800

    Initial commit

---
+++ js/progressBar.js
@@ -0,0 +1,12 @@
+var bars = document.querySelectorAll('.meter > span');
+console.clear();
+
+setInterval(function(){
+  bars.forEach(function(bar){
+    var getWidth = parseFloat(bar.dataset.progress);
+    
+    for(var i = 0; i < getWidth; i++) {
+      bar.style.width = i + '%';
+    }
+  });
+}, 500);
